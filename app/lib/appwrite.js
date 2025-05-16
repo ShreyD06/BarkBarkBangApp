c8508faacb896f4d51099a12cb887807c7a4f16c
shreyd06 <shreyder.desai@gmail.com>
2025-05-16 17:43:57 -0400

    function changes

--- app/lib/appwrite.js
+++ app/lib/appwrite.js
@@ -1,1 +1,6 @@
+import { Client, Account } from 'react-native-appwrite';
 
+const client = new Client()
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject('6827a46100150290dfd7')
+    .setPlatform('com.shreyd.bbb');

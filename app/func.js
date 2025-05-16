90caabc287f11b584d2baf086def6b116813c47f
shreyd06 <shreyder.desai@gmail.com>
2025-05-16 17:42:15 -0400

    function changes

---
+++ app/func.js
@@ -0,0 +1,16 @@
+
+
+// module.exports = async ({ req, res, log }) => {
+//     context.log("Received Google Sheets update");
+//     context.log(req.body); // Logs the data Google sent
+  
+//     return res.json({ success: true, message: "Data received!" });
+//   };
+
+export default async ({req, res, log, error}) => {
+    log("received update")
+    log(req.bodyJson)
+
+    return res.json({ success: true, message: "Data received!" })
+}
+  
